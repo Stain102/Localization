7f2026636a56b23ffed77dc74a859e046d35f645
Patrik Johansen <Stain102@gmail.com>
2020-03-08 22:35:04 +0100

    Editor window
    Added quick empty editor window.

---
+++ Localization/Assets/Localization/Editor/TextLocalizationEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+public class TextLocalizationEditWindow : EditorWindow
+{
+    [MenuItem("Window/Localization")]
+    public static void Open()
+    {
+        GetWindow<TextLocalizationEditWindow>("Localization");
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Hello"))
+        {
+            Debug.Log("Hello");
+        }
+    }
+}
